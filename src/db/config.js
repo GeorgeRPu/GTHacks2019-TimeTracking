63b69ceb513eeaa5b4d9a487f3f9661494d2dc97
GeorgeRPu <pu.george99@gmail.com>
2021-10-16 01:21:15 -0400

    Heroku static build

--- src/db/config.js
+++ src/db/config.js
@@ -1,3 +1,12 @@
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);  // eslint-disable-line
+const firebaseConfig = {
+    apiKey: "AIzaSyCNKGCYFX8EvfRLaTpozHU5bzYJtrXbvlE",
+    authDomain: "time-tracker-257113.firebaseapp.com",
+    databaseURL: "https://time-tracker-257113.firebaseio.com",
+    projectId: "time-tracker-257113",
+    storageBucket: "time-tracker-257113.appspot.com",
+    messagingSenderId: "599606899630",
+    appId: "1:599606899630:web:c34691cb641b3ab8804e3f",
+    measurementId: "G-3D20EFRN7D"
+};
 
 export { firebaseConfig };
